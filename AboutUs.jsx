73e14654fb9a8cbc3cb85dcdd128049c5437875d
jadavshreya16-sketch <jadavshreya16@gmail.com>
2026-04-17 18:18:24 +0530

    Create AboutUs.jsx

---
+++ AboutUs.jsx
@@ -0,0 +1,10 @@
+function AboutUs() {
+  return (
+    <div>
+      <h1>Paradise Nursery</h1>
+      <p>We provide beautiful indoor plants for your home.</p>
+    </div>
+  );
+}
+
+export default AboutUs;
